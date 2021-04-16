3af6905a6f463751f93d1962334d5377d7a462fa
Jonny Graybill <jon.a.graybill@gmail.com>
2021-04-16 11:55:22 -0700

    added food for snake

--- snake/sketch.js
+++ snake/sketch.js
@@ -1,26 +1,40 @@
 
 let snake;
+let food;
+let cols;
+let rows;
 const SCL = 20;
 
 function setup() {
   createCanvas(1400, 700);
-  snake = new Snake()
+  snake = new Snake();
+  frameRate(10);
+  cols = floor(width / SCL);
+  rows = floor(height / SCL);
+  pickFoodLocation();
 };
 
 function draw() {
   background(51);
   snake.update();
   snake.show();
+  fill(255, 0, 0);
+  rect(food.x, food.y, SCL, SCL);
 };
 
 function keyPressed() {
   if (keyCode === UP_ARROW) {
-    snake.moveDir(0,-1);
+    snake.moveDir(0, -1);
   } else if (keyCode === DOWN_ARROW) {
-    snake.moveDir(0,1);
+    snake.moveDir(0, 1);
   } else if (keyCode === LEFT_ARROW) {
-    snake.moveDir(-1,0);
+    snake.moveDir(-1, 0);
   } else if (keyCode === RIGHT_ARROW) {
-    snake.moveDir(1,0);
+    snake.moveDir(1, 0);
   }
+}
+
+function pickFoodLocation() {
+  food = createVector(floor(random(cols)), floor(random(rows)));
+  food.mult(SCL);
 }
